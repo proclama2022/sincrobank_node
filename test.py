6f3e7f4e2c2142f7a3afc3221a769ff5ef04e0d0
proclama2022 <132828300+proclama2022@users.noreply.github.com>
2024-06-25 12:04:43 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,13 +4,6 @@ def sum_even_numbers(numbers):
     for num in numbers:
         if num % 2 == 0:
             even_numbers.append(num)
-    
-    # Secondo ciclo per calcolare la somma dei numeri pari
-    total_sum = 0
-    for even in even_numbers:
-        total_sum += even
-
-    return total_sum
 
 # Lista di numeri per testare la funzione
 test_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
